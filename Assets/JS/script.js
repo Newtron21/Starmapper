97f7374c472c2f9236e74ca0fef9bcca68e629f3
Newtron21 <annacpnewton@gmail.com>
2023-07-27 20:00:51 -0600

    adding instructions

---
+++ Assets/JS/script.js
@@ -0,0 +1,47 @@
+//var latitude=latitude
+//var longitude=longitude
+//var date=date
+//var starImage=returned star image
+
+
+
+//TASK TO DO FIRST SUBMIT
+//have variable that contains current date
+//var date=date
+//event listener to listen for submit
+//---when submit insert loading bar 
+//take submit info turn into variable
+
+
+//TASK 2 saving and rendering info
+//save submit info into local storage and render them underneath submit bar
+//make sure it saves to page when page is refreshed 
+
+//TASK 3 fetch request through mapquest with said variables 
+//take variable run through mapquest fetch 
+// fetch first time through mapquest 
+//put in location/city and get out lat and long
+//creates new variable that contains latitude and longitude
+//var latitude=latitude
+//var longitude=longitude
+//---we might have to change lat and long into string so the variable pulls correctly from next fetch
+
+
+//task 4 
+//take variables and put it in next fetch
+//fetch for second 
+//that gives information which we load on page
+//creates variable 
+//var starImage=returned star image
+
+//TASK 5 
+//get rid of "hidden" on image class to show image
+//put image into page in specific spot 
+
+
+//--when this fetch comes back turn off loading bar and replace with image 
+
+//---once this is done take variables from above and put them into storage and render them underneath submit bar
+
+//---fetch 3, 4 , 5 places and put underneath the large image
+//---also render information in 3 other places in columns 
